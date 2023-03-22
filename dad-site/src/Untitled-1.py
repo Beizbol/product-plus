5327b862fcc9578130e36d1277f86c37f5b5e47c
Jake Surrett <30479707+Beizbol@users.noreply.github.com>
2023-03-22 17:30:11 -0400

    v3

---
+++ dad-site/src/Untitled-1.py
@@ -0,0 +1,5 @@
+brandstr = "Austere, JBL, JBL Synthesis, Revel, Arcam, Audiolab, Mission, Dual, Thorens, Flexson, Harman Kardon, Human Touch, Mark Levinson, Marshall, One for All, Velodyne, Jaymar, JBL Professional, Crown, AKG, DBX, Soundcraft, Totem, KIN by Totem, FiiO"
+
+brands = brandstr.split(", ")
+
+print(brands.sort())
