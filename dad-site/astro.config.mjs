5749c91005d5c07c26c7285d617bb555366c8f30
Beizbol <30479707+Beizbol@users.noreply.github.com>
2026-03-04 21:47:43 -0500

    push v26

--- dad-site/astro.config.mjs
+++ dad-site/astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 // https://astro.build/config
-import sitemap from "@astrojs/sitemap";
+// import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://product-plus.ca',
-  integrations: [sitemap()],
+  integrations: [], //sitemap()
   build: {
     assets: 'assets'
   }
